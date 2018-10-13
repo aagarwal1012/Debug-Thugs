c1a056ec7c2a34fcd56b5fce9b7a55de42e0df13
codekaust <kaustubh1303@gmail.com>
2018-10-13 19:03:29 +0530

    initialisedAndroidApp

---
+++ androidApp/GreenWay/app/src/main/java/debugthugs/mdgiitr/com/greenway/MainActivity.java
@@ -0,0 +1,13 @@
+package debugthugs.mdgiitr.com.greenway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
