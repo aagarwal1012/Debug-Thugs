3274db746c4143d7f6a643a19d1feedc6dc8c0ab
aagarwal1012 <aagarwal9782@gmail.com>
2018-10-14 12:52:22 +0530

    finalized map activity

--- androidApp/GreenWay/app/src/main/java/debugthugs/mdgiitr/com/greenway/MapsActivity.java
+++ androidApp/GreenWay/app/src/main/java/debugthugs/mdgiitr/com/greenway/MapsActivity.java
@@ -22,6 +22,7 @@ import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.exception.ApolloException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -40,6 +41,7 @@ import com.google.maps.model.EncodedPolyline;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 
@@ -67,9 +69,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         setActionBar(toolbar);
 
         Criteria criteria = new Criteria();
-        mprovider = locationManager.getBestProvider(criteria, false);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mprovider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         location = locationManager.getLastKnownLocation(mprovider);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         locationManager.requestLocationUpdates(mprovider, 50, (float) 0.01, this);
 
         googlelist = new ArrayList<>();
@@ -77,7 +99,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         progressDialog = new ProgressDialog(MapsActivity.this);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.maps);
         mapFragment.getMapAsync(this);
 
@@ -130,78 +152,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         getPoints();
 
-        mMap = googleMap;
-
-        LatLng barcelona = new LatLng(41.385064,2.173403);
-        mMap.addMarker(new MarkerOptions().position(barcelona).title("Marker in Barcelona"));
-
-        LatLng madrid = new LatLng(40.416775,-3.70379);
-        mMap.addMarker(new MarkerOptions().position(madrid).title("Marker in Madrid"));
-
-        LatLng zaragoza = new LatLng(41.648823,-0.889085);
-
-        //Define list to get all latlng for the route
-        List<LatLng> path = new ArrayList();
-
-
-        //Execute Directions API request
-        GeoApiContext context = new GeoApiContext.Builder()
-                .apiKey("AIzaSyAzTd_IUCrQxrlDh9mHcvm2xYl039tg5xk")
-                .build();
-        DirectionsApiRequest req = DirectionsApi.getDirections(context, "41.385064,2.173403", "40.416775,-3.70379");
-        try {
-            DirectionsResult res = req.await();
-
-            //Loop through legs and steps to get encoded polylines of each step
-            if (res.routes != null && res.routes.length > 0) {
-                DirectionsRoute route = res.routes[0];
-
-                if (route.legs !=null) {
-                    for(int i=0; i<route.legs.length; i++) {
-                        DirectionsLeg leg = route.legs[i];
-                        if (leg.steps != null) {
-                            for (int j=0; j<leg.steps.length;j++){
-                                DirectionsStep step = leg.steps[j];
-                                if (step.steps != null && step.steps.length >0) {
-                                    for (int k=0; k<step.steps.length;k++){
-                                        DirectionsStep step1 = step.steps[k];
-                                        EncodedPolyline points1 = step1.polyline;
-                                        if (points1 != null) {
-                                            //Decode polyline and add points to list of route coordinates
-                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
-                                            for (com.google.maps.model.LatLng coord1 : coords1) {
-                                                path.add(new LatLng(coord1.lat, coord1.lng));
-                                            }
-                                        }
-                                    }
-                                } else {
-                                    EncodedPolyline points = step.polyline;
-                                    if (points != null) {
-                                        //Decode polyline and add points to list of route coordinates
-                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
-                                        for (com.google.maps.model.LatLng coord : coords) {
-                                            path.add(new LatLng(coord.lat, coord.lng));
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        } catch(Exception ex) {
-            Log.e(TAG, ex.getLocalizedMessage());
-        }
-
-        //Draw the polyline
-        if (path.size() > 0) {
-            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
-            mMap.addPolyline(opts);
-        }
-
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zaragoza, 6));
+//        mMap = googleMap;
+//
+//        LatLng barcelona = new LatLng(41.385064,2.173403);
+//        mMap.addMarker(new MarkerOptions().position(barcelona).title("Marker in Barcelona"));
+//
+//        LatLng madrid = new LatLng(40.416775,-3.70379);
+//        mMap.addMarker(new MarkerOptions().position(madrid).title("Marker in Madrid"));
+//
+//        LatLng zaragoza = new LatLng(41.648823,-0.889085);
+//
+//        //Define list to get all latlng for the route
+//        List<LatLng> path = new ArrayList();
+//
+//
+//        //Execute Directions API request
+//        GeoApiContext context = new GeoApiContext.Builder()
+//                .apiKey("AIzaSyAzTd_IUCrQxrlDh9mHcvm2xYl039tg5xk")
+//                .build();
+//        DirectionsApiRequest req = DirectionsApi.getDirections(context, "41.385064,2.173403", "40.416775,-3.70379");
+//        try {
+//            DirectionsResult res = req.await();
+//
+//            //Loop through legs and steps to get encoded polylines of each step
+//            if (res.routes != null && res.routes.length > 0) {
+//                DirectionsRoute route = res.routes[0];
+//
+//                if (route.legs !=null) {
+//                    for(int i=0; i<route.legs.length; i++) {
+//                        DirectionsLeg leg = route.legs[i];
+//                        if (leg.steps != null) {
+//                            for (int j=0; j<leg.steps.length;j++){
+//                                DirectionsStep step = leg.steps[j];
+//                                if (step.steps != null && step.steps.length >0) {
+//                                    for (int k=0; k<step.steps.length;k++){
+//                                        DirectionsStep step1 = step.steps[k];
+//                                        EncodedPolyline points1 = step1.polyline;
+//                                        if (points1 != null) {
+//                                            //Decode polyline and add points to list of route coordinates
+//                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+//                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+//                                                path.add(new LatLng(coord1.lat, coord1.lng));
+//                                            }
+//                                        }
+//                                    }
+//                                } else {
+//                                    EncodedPolyline points = step.polyline;
+//                                    if (points != null) {
+//                                        //Decode polyline and add points to list of route coordinates
+//                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+//                                        for (com.google.maps.model.LatLng coord : coords) {
+//                                            path.add(new LatLng(coord.lat, coord.lng));
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        } catch(Exception ex) {
+//            Log.e(TAG, ex.getLocalizedMessage());
+//        }
+//
+//        //Draw the polyline
+//        if (path.size() > 0) {
+//            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+//            mMap.addPolyline(opts);
+//        }
+//
+//        mMap.getUiSettings().setZoomControlsEnabled(true);
+//
+//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zaragoza, 6));
     }
 
     void getPoints() {
